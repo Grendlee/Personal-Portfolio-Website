dc11568cc8ebc1f6002a920329b921e8ad77dbfc
Grantley Kuo <grantley@Grantleys-MacBook-Air.local>
2023-01-23 21:56:22 -0800

    first commit

---
+++ logicForContact.js
@@ -0,0 +1,4 @@
+function alertUser(form)
+{
+    alert("Submition Complete.\n\nFirst Name: " + form["fname"].value + "\n" + "Last Name: " + form["lname"].value + "\n" + "Email: " + form["email"].value + "\n" + "Comments: " + form["comments"].value);
+}
