f52c8a487ffd1f7282944a9dca40a30118fdb08f
Grendlee <Grantley.kuo@gmail.com>
2025-02-19 09:32:53 -0800

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Portfolio/', // Change this to your GitHub repo name
+  base: '/Personal-Portfolio-Website/', // Change this to your GitHub repo name
 });
